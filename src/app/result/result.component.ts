f77f9da0ac25fb9287f1d9fd2f100c013e37b694
utkarshachetanpatil <33398024+utkarshachetanpatil@users.noreply.github.com>
2021-05-20 22:29:40 +0100

    c

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {SessionService} from '../session.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-result',
@@ -8,12 +9,25 @@ import {SessionService} from '../session.service';
 })
 export class ResultComponent implements OnInit {
 
-  constructor(private sessionService: SessionService) { }
+  constructor(private sessionService: SessionService, private router: Router) { }
 
   ngOnInit(): void {
+    if ( !this.sessionService.customerDetail) {
+      this.router.navigate(['/']);
+    }
   }
 
-  get resultString() {
-    return JSON.stringify(this.sessionService.customerDetail);
+  get userName(): string {
+    return this.sessionService.customerDetail && this.sessionService.customerDetail.firstName ? this.sessionService.customerDetail.firstName : '';
+  }
+
+  camel2title = (camelCase: string) => camelCase.replace(/([A-Z])/g, (match) => ` ${match}`).replace(/^./, (match) => match.toUpperCase()).trim();
+
+  get attributeNames(): string[] {
+    return Object.keys(this.sessionService.customerDetail);
+  }
+
+  getValue(attribute: string): string {
+    return this.sessionService.customerDetail[attribute];
   }
 }
